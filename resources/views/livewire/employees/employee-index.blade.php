ddc75b67930e613868c62bb3aa36bbbee814dccd
agriapriliando <agripriliando@gmail.com>
2025-06-18 13:48:06 +0700

    img

--- resources/views/livewire/employees/employee-index.blade.php
+++ resources/views/livewire/employees/employee-index.blade.php
@@ -215,12 +215,12 @@
                             <!-- table -->
                             <div class="mb-4 mb-md-5">
                                 <hr class="w-50 mb-3 border-dark-subtle">
-                                <address class="m-0 text-secondary">Seluruh data ini diakses oleh HDI Kemenag secara
-                                    <b>Real Time</b> <br> memakai API berikut :
+                                <address class="m-0 text-secondary">Seluruh data ini diakses oleh Kemenag Pusat secara
+                                    <b>Real Time</b> <br> memakai URL API berikut :
                                 </address>
-                                <a href="">URL : _____</a>
+                                <img src="{{ asset('cara_login.png') }}" alt="cara_login" class="img-fluid mt-2">
                             </div>
-                            <div class="row mb-sm-4 mb-md-5">
+                            <div class="row mb-sm-4 mb-md-5 d-none">
                                 <div class="col-12 col-sm-6">
                                     <div class="mb-4 mb-sm-0">
                                         <div class="mb-3 text-primary">
@@ -257,28 +257,6 @@
                                                 <a class="link-secondary text-decoration-none" href="mailto:demo@yourdomain.com">demo@yourdomain.com</a>
                                             </p>
                                         </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <div class="mb-3 text-primary">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-alarm" viewBox="0 0 16 16">
-                                        <path d="M8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5z" />
-                                        <path
-                                            d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z" />
-                                    </svg>
-                                </div>
-                                <div>
-                                    <h4 class="mb-2">Opening Hours</h4>
-                                    <p class="mb-2">Explore our business opening hours.</p>
-                                    <hr class="w-50 mb-3 border-dark-subtle">
-                                    <div class="d-flex mb-1">
-                                        <p class="text-secondary fw-bold mb-0 me-5">Mon - Fri</p>
-                                        <p class="text-secondary mb-0">9am - 5pm</p>
-                                    </div>
-                                    <div class="d-flex">
-                                        <p class="text-secondary fw-bold mb-0 me-5">Sat - Sun</p>
-                                        <p class="text-secondary mb-0">9am - 2pm</p>
                                     </div>
                                 </div>
                             </div>
